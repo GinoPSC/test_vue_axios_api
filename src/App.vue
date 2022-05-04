a6af369e20581b0d781e53bec103983c1aa8dcd0
GinoPSC <92866955+GinoPSC@users.noreply.github.com>
2022-05-04 01:24:53 -0400

    axios post test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">GET</router-link> |
+    <router-link to="/about">POST</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+  <h1>{{titulo}}</h1>
+  <div class='api_table_post'>
+    <table>
+      <h3>Test de estado</h3>
+      <p>{{postStatus}}</p>
+    </table>
   </div>
 </template>
+
+<script>
+
+import axios from 'axios'
+export default {
+  name: 'HomeView',
+  data(){
+    return{
+      titulo: 'Test api consumption with axios',
+      postStatus: null
+    }
+  },
+  created(){
+    let url = 'https://jsonplaceholder.typicode.com/posts';
+    let posteada = {
+      title: "hola",
+      body: "mundo"
+    };
+
+    axios.post(url,posteada)
+    .then(response => {
+      this.postStatus = response
+    });
+  }
+}
+
+</script>
+
+<style>
+  .api_table_post table{
+    margin: auto;
+  }
+</style>
